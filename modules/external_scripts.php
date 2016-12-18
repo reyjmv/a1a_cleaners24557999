dd66b9dfe8cafe4cc25de4e645ee303b75186891
Reinaldo Jose Mejia Villegas <reyj.m.v@gmail.com>
2016-12-17 21:31:23 -0400

    version inicial del proyecto

---
+++ modules/external_scripts.php
@@ -0,0 +1,13 @@
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="js/jquery-3.1.1.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/min/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.js"></script>
+    <script src="js/plugins.js"></script>
+
+        <!-- Google Map -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeY687WZhl8o3abflENujghcvAtsueKWM&callback=initMap"
+  type="text/javascript"></script>
+    <script src="js/google-map-init.js"></script>
+    <script src="js/main.js"></script>
---
+++ work.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Space template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    
+    <!-- CSS -->
+<?php 
+  include("modules/external_css.php");
+?>
+    <!-- Js -->
+<?php 
+  include("modules/external_scripts.php");
+?>
+
+  </head>
+  <body>
+
+
+    <!-- Header Start -->
+<?php 
+  include("modules/header.php");
+?>
+    
+        <!-- Slider Start -->
+    <section id="global-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block">
+                        <h1>Just some of our latest projects.</h1>
+                        <p>Don’t just take our word for it. Check out some of our latest work.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        <!-- Portfolio Start -->
+    <section id="portfolio-work">
+        <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="block">
+                  <div class="portfolio-menu">
+                    <ul>
+                        <li class="filter" data-filter="all">Everything</li>
+                        <li class="filter" data-filter=".Branding">Branding</li>
+                        <li class="filter" data-filter=".Websites">Websites</li>
+                        <li class="filter" data-filter=".Graphic">Graphic</li>
+                        <li class="filter" data-filter=".design">design</li>
+                        <li class="filter" data-filter=".Video">Video</li>
+                    </ul>
+                  </div>
+                  <div class="portfolio-contant">
+                    <ul id="portfolio-contant-active">
+                        <li class="mix Branding">
+                          <a href="#">
+                            <img src="img/portfolio/work1.jpg" alt="">
+                            <div class="overly">
+                              <div class="position-center">
+                                <h2>Tesla Motors</h2>
+                                <p>Labore et dolore magna aliqua. Ut enim ad </p>
+                                
+                              </div>
+                            </div>
+                          </a>
+                      </li>
+                      <li class="mix Websites">
+                        <a href="#">
+                          <img src="img/portfolio/work2.jpg" alt="">
+                          <div class="overly">
+                            <div class="position-center">
+                              <h2>Tesla Motors</h2>
+                              <p>Labore et dolore magna aliqua. Ut enim ad </p>
+                              
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li class="mix Graphic">
+                        <a href="#">
+                          <img src="img/portfolio/work3.jpg" alt="">
+                          <div class="overly">
+                            <div class="position-center">
+                              <h2>Tesla Motors</h2>
+                              <p>Labore et dolore magna aliqua. Ut enim ad </p>
+                              
+                            </div>
+                          </div>
+                          
+                        </a>
+                      </li>
+                      <li class="mix design">
+                        <a href="#">
+                          <img src="img/portfolio/work4.jpg" alt="">
+                          <div class="overly">
+                            <div class="position-center">
+                              <h2>Tesla Motors</h2>
+                              <p>Labore et dolore magna aliqua. Ut enim ad </p>
+                              
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li class="mix Video">
+                        <a href="#">
+                          <img src="img/portfolio/work5.jpg" alt="">
+                          <div class="overly">
+                            <div class="position-center">
+                              <h2>Tesla Motors</h2>
+                              <p>Labore et dolore magna aliqua. Ut enim ad </p>
+                              
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                      <li class="mix Graphic">
+                        <a href="#">
+                          <img src="img/portfolio/work6.jpg" alt="">
+                          <div class="overly">
+                            <div class="position-center">
+                              <h2>Tesla Motors</h2>
+                              <p>Labore et dolore magna aliqua. Ut enim ad </p>
+                              
+                            </div>
+                          </div>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Clients Logo Section Start -->
+    <section id="clients-logo-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block">
+              <div id="clients-logo" class="owl-carousel">
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo1.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo2.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo3.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo4.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo5.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo3.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo2.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo5.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo1.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo4.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo5.png" alt="Features">
+                </div>
+                <div class="clients-logo-img">
+                  <img src="img/clients/clients-logo3.png" alt="Features">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+<!-- Call to action Start -->
+    <section id="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block">
+              <h2>We design delightful digital experiences.</h2>
+              <p>Read more about what we do and our philosophy of design. Judge for yourself The work and results we’ve achieved for other clients, and meet our highly experienced Team who just love to design.</p>
+              <a class="btn btn-default btn-call-to-action" href="#" >Tell Us Your Story</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- footer Start -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <div class="footer-manu">
+                    <ul>
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#">How it works</a></li>
+                      <li><a href="#">Support</a></li>
+                      <li><a href="#">Terms</a></li>
+                    </ul>
+                  </div>
+                  <p>Copyright &copy; Crafted by <a href="https://dcrazed.com/">Dcrazed</a>.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+        
+    </body>
+</html>
